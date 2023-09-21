5463d6c1c72c5f45aee8dd7f62de90c326deb6dd
Gordon Lei <glei@andrew.cmu.edu>
2023-09-21 11:45:02 -0700

    moved code and added comments to React components

--- components/inventory.jsx
+++ components/inventory.jsx
@@ -1,42 +1,64 @@
+/*
+This component is the 
+*/
+
+//  libraries
 import Image from "next/image";
 import Shop from "./shop";
+import { current } from "@reduxjs/toolkit";
 import { useState, useRef } from "react";
-//
+//  react-redux
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, inventorySelector } from "../store/inventorySlice";
-import { current } from "@reduxjs/toolkit";
-//
 
+//  Inventory component
 export default function Inventory({
   /* inventory, */
   setOverflowScroll,
   setHidden
   /* setInventory */
 }) {
+  /*
+  Vars
+  */
+
+  //  use the inventory slice
+  const currentInventory = useSelector(inventorySelector);
+  //  not exactly  sure if this is necessary
   const buttonOpenShop = useRef(null);
+  //  react states
   //  might have to move this to the shop
   const [showShop, setShopVisibility] = useState(false);
+
+  /*
+  Functions
+  */
+
+  //  handle setting the shop to be visible
   const handleClick = async (event) => {
     //  show the shop
     setShopVisibility(!showShop);
     setOverflowScroll(false);
     setHidden();
   };
-  const currentInventory = useSelector(inventorySelector);
 
+  /*
+  return the component
+  */
   return (
     <div>
+      {/* The Shop component */}
       <div className="absolute left-0 flex">
         <div className="flex">
           <Shop showShop={showShop} />
-
           <div className="sticky top-16 h-20 content-center"></div>
         </div>
       </div>
 
-      <div className="sticky top-16  h-20  content-center">
-        {/* Item Section */}
+      <div className="sticky top-n6  h-20  content-center">
+        {/* Inventory Section */}
         {currentInventory.map((item, index) => {
+          /* Map all of the item icons */
           if (item.icon) {
             return (
               <img
@@ -56,6 +78,7 @@ export default function Inventory({
             );
           }
         })}
+        {/* Button to open the shop menu */}
         <button idname="openShop">
           <Image
             src="/images/temp_openInventory.png"
--- components/shop.jsx
+++ components/shop.jsx
@@ -1,16 +1,14 @@
+/*
+
+This component is that shop pop up where users can purchase and sell items
+
+*/
+
+//  libraries
 import Image from "next/image";
 import { useState, useEffect } from "react";
-import { ReactReduxContext } from "react-redux";
 import useSWRImmutable from "swr/immutable";
-import { useDispatch, useSelector } from "react-redux";
-//
-import {
-  addItem,
-  removeItemById,
-  inventorySelector
-} from "../store/inventorySlice";
-import { addStats, removeStats } from "../store/statsSlice";
-//
+//  helper functions
 import { getAllItemInfo } from "../helper/lolstaticdata";
 import {
   generateInventoryComponentInfo,
@@ -19,8 +17,15 @@ import {
 } from "../helper/lolItem";
 import { objectMapArray } from "../helper/misc";
 import { validateInventory } from "../helper/lolItem";
-
-//
+//  react-redux
+import { ReactReduxContext } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addItem,
+  removeItemById,
+  inventorySelector
+} from "../store/inventorySlice";
+import { addStats, removeStats } from "../store/statsSlice";
 
 //Write a fetcher function to wrap the native fetch function and return the result of a call to url in json format
 const fetcher = (url) =>
@@ -35,6 +40,7 @@ const fetcher = (url) =>
       return data;
     });
 
+//  Shop component
 export default function Shop({
   showShop,
   /* setInventory, */
@@ -43,8 +49,20 @@ export default function Shop({
   currentItems
 }) {
   /*
-  need something to update stats
+  Vars
   */
+
+  //  react-redux to use inventory slice
+  const dispatch = useDispatch();
+  const currentInventory = useSelector(inventorySelector);
+  //  react  states
+  const [currItem, setCurrItem] = useState(-1);
+  const [parsedItemData, setParsedItemData] = useState({});
+  /*
+  Functions
+  */
+
+  //  from the array of items, add it into a dict where the key is the item ID
   const parseData = (itemArray) => {
     console.log(itemArray);
     const dataDict = {};
@@ -53,15 +71,6 @@ export default function Shop({
     });
     return dataDict;
   };
-  const dispatch = useDispatch();
-  const currentInventory = useSelector(inventorySelector);
-
-  const [currItem, setCurrItem] = useState(-1);
-  const [parsedItemData, setParsedItemData] = useState({});
-  //
-
-  //
-  //  let itemData;
   //Set up SWR to run the fetcher function when calling "/api/staticdata"
   //There are 3 possible states: (1) loading when data is null (2) ready when the data is returned (3) error when there was an error fetching the data
   const { itemData, error } = useSWRImmutable("/api/itemData", fetcher, {
@@ -74,13 +83,12 @@ export default function Shop({
       console.log(parsedData);
     }
   });
-  //  let itemData;
-  //
+  //  handle pressing the item and setting the current selected item to that
   const handleClick = (itemId) => {
     console.log("CLICKED", itemId);
     setCurrItem(itemId);
   };
-
+  //  handle buying the selected item
   const handlePurchase = (event, itemId) => {
     event.stopPropagation();
     console.log(itemId);
@@ -94,7 +102,7 @@ export default function Shop({
 
     /* setInventory(generateInventoryComponentInfo()); */
   };
-
+  //  handle selling the selected item
   const handleSell = (event, itemId) => {
     event.stopPropagation();
     console.log(itemId);
@@ -106,42 +114,17 @@ export default function Shop({
       dispatch(removeStats(item));
     }
   };
-
-  //
+  //  from the item dict, get the item information based on a key/property of the item
   const getItemInfo = (id, param) => {
     const item = parsedItemData[id.toString()];
     //  console.log(item);
     return item[param];
   };
-  /*
-  const getItemData = async () => {
-    let returnData;
-    const latestItemJsonURL =
-      process.cwd() +
-      `/data/lolstaticdata/${process.env.leaguePatch}/items.json`;
-    fetch(latestItemJsonURL).then(() => {});
 
-    if (existsSync(latestItemJsonURL)) {
-      const data = readFileSync(latestItemJsonURL);
-      returnData = JSON.parse(data);
-    } else {
-      returnData = await getAllItemInfo();
-    }
-    return returnData;
-  };
-  */
   /*
-  const url = "localhost:3000";
-  useEffect(() => {
-    fetch(`/api/json/${process.env.leaguePatch}/items.json`)
-      .then((res) => {
-        itemData = res.json();
-      })
-      .catch(async (res) => {
-        itemData = await getAllItemInfo();
-      });
-  }, []);
+  Return the component
   */
+
   //Handle the error state
   if (error && showShop) return <div>Failed to load</div>;
   //Handle the loading state
@@ -152,6 +135,7 @@ export default function Shop({
     return (
       <div className="flex overscroll-auto fixed ">
         {/* This is the shop window */}
+        {/* Left side panel that shows all the items */}
         <div className="grid grid-cols-9 gap-2 bg-slate-500">
           <div className="sticky h-screen  top-16 overflow-y-scroll overscroll-contain col-span-5 grid grid-cols-5 gap-2">
             {parsedItemData &&
@@ -166,8 +150,9 @@ export default function Shop({
                 );
               })}
           </div>
-          {/* This is the item description */}
+          {/* Right side panel that shows selected item description */}
           <div className="sticky w-full col-span-4 pr-32">
+            {/* If an item is selected, then display it*/}
             {currItem < 0 || (
               <div>
                 <img
@@ -177,33 +162,27 @@ export default function Shop({
                 />
                 <div>{getItemInfo(currItem, "name")}</div>
                 <div>{getItemInfo(currItem, "simpleDescription")}</div>
-
-                {
-                  /*
-                  Need to show the stats 
-                  */
-
-                  // console.log(getItemInfo(currItem, "stats"));
-                  objectMapArray(
-                    extractItemStatFromDict(getItemInfo(currItem, "stats")),
-                    (statName, value) => {
-                      return (
-                        <div key={`${statName}`}>{`${value} ${statName}`}</div>
-                      );
-                    }
-                  )
-                }
-
+                {/* Show the stats obtainable from buying the item */}
+                {objectMapArray(
+                  extractItemStatFromDict(getItemInfo(currItem, "stats")),
+                  (statName, value) => {
+                    return (
+                      <div key={`${statName}`}>{`${value} ${statName}`}</div>
+                    );
+                  }
+                )}
+                {/* Map through all the item passives */}
                 {getItemInfo(currItem, "passives").map((currentPassive, id) => {
                   /*
-              Need a part to show Mythic Passives
-              */
+                  Need a part to show Mythic Passives stats
+                  */
                   return (
                     <div key={`${currentPassive.name}-${id}`}>
                       {currentPassive.name}: {currentPassive.effects}
                     </div>
                   );
                 })}
+                {/* Map through all the item actives */}
                 {getItemInfo(currItem, "active").map((currentActive, id) => {
                   return (
                     <div key={`${currentActive.name}-${id}`}>
@@ -211,6 +190,7 @@ export default function Shop({
                     </div>
                   );
                 })}
+                {/* Button to buy the item */}
                 <div
                   onClick={(e) => {
                     handlePurchase(e, getItemInfo(currItem, "id"));
@@ -218,6 +198,7 @@ export default function Shop({
                 >
                   Purchase
                 </div>
+                {/* Button to sell the item  */}
                 <div
                   onClick={(e) => {
                     handleSell(e, getItemInfo(currItem, "id"));
