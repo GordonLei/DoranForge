b3fd1b5c550e878b220f539703bcb9beda1bfdb6
Gordon Lei <glei@andrew.cmu.edu>
2024-06-13 00:22:28 -0400

    remove API key from next config

---
+++ next.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    //  dataDragon
+    leaguePatch: "14.4.1",
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ddragon.leagueoflegends.com",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "ddragon.leagueoflegends.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.communitydragon.org",
+        port: "",
+      },
+      {
+        protocol: "http",
+        hostname: "raw.communitydragon.org",
+        port: "",
+      },
+    ],
+  },
+};
